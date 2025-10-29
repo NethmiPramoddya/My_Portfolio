2624c7a16a02266cca48f058df74c79d07f1fa5f
NethmiPramoddya <nethmik2001@gmail.com>
2025-10-29 17:58:45 +0530

    made UI changes

--- src/components/HireMeSection.jsx
+++ src/components/HireMeSection.jsx
@@ -9,25 +9,29 @@ const HireMeSection = () => {
   };
 
   return (
-    <section className="bg-gradient-to-r from-indigo-500 to-purple-600 hover:-translate-y-1 text-white py-16 px-4 sm:px-6 md:px-20 rounded-xl h-auto sm:h-[100px] flex items-center justify-center mx-5">
-      <div className="container flex flex-col items-center justify-between mx-auto md:flex-row">
-        {/* Text Section */}
-        <div className="mb-6 text-center md:mb-0 md:text-left">
-          <h2 className="mb-2 text-xl font-bold md:text-3xl">
-            Want to work with me?
-          </h2>
-          <p className="text-lg text-gray-300">
-            Always feel free to contact & hire me
-          </p>
-        </div>
+    <section className="px-6 pt-8 pb-16">
+      <div className="max-w-6xl mx-auto">
+        <div className="px-8 py-12 text-white transition-transform duration-300 shadow-xl bg-gradient-to-r from-indigo-500 to-purple-600 rounded-2xl hover:-translate-y-1">
+          <div className="flex flex-col items-center justify-between mx-auto md:flex-row">
+            {/* Text Section */}
+            <div className="mb-6 text-center md:mb-0 md:text-left">
+              <h2 className="mb-2 text-xl font-bold md:text-3xl text-white">
+                Want to work with me?
+              </h2>
+              <p className="text-lg text-gray-200">
+                Always feel free to contact & hire me
+              </p>
+            </div>
 
-        {/* Button */}
-        <button
-          onClick={handleHireClick}
-          className="bg-white text-[#0b2651] font-semibold px-8 py-3 rounded-md shadow-md hover:bg-gray-100 transition"
-        >
-          Hire Me
-        </button>
+            {/* Button */}
+            <button
+              onClick={handleHireClick}
+              className="bg-white text-indigo-600 font-semibold px-8 py-3 rounded-2xl shadow-md hover:-translate-y-1 hover:bg-gray-100 transition-all duration-300"
+            >
+              Hire Me
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
